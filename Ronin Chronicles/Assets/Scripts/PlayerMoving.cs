d2fe91849686e422d744436ebfa565e044780cae
aromansky <kirbi.zsthan@gmail.com>
2022-11-23 00:19:45 +0300

    Updating the script, a new object in the TestScene and "connecting" the animator
    -Updated player movement script
    -Add "Springboard" to TestScene
    - "Connect" the animation of walking and running (not finalized)

--- Ronin Chronicles/Assets/Scripts/PlayerMoving.cs
+++ Ronin Chronicles/Assets/Scripts/PlayerMoving.cs
@@ -4,20 +4,50 @@ public class PlayerMoving : MonoBehaviour
 {
     public float speed;
     public float speedMultiplier;
+    public float gravity;
+
+    private CharacterController _charController;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+        gravity *= gravity < 0 ? 1: -1;
+    }
 
     void Update()
     {
-        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float deltaZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.Translate(deltaX, 0, deltaZ);
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+     
+        Vector3 movement = new Vector3(deltaX, gravity, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             speed *= speedMultiplier;
+            _animator.SetBool("Run", true);
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             speed /= speedMultiplier;
+            _animator.SetBool("Run", false);
         }
+
+        if (deltaX != 0 || deltaZ != 0)
+        {
+            _animator.SetBool("Move", true);
+        }
+        else
+        {
+            _animator.SetBool("Move", false);
+        }
+        
     }
 }
