cb5f7a22d3c5a8682dac63bad8a75d17b4f10ee9
aromansky <kirbi.zsthan@gmail.com>
2022-12-10 22:21:36 +0300

    Delete comments

--- Ronin Chronicles/Assets/Scripts/EnemyMoving.cs
+++ Ronin Chronicles/Assets/Scripts/EnemyMoving.cs
@@ -3,30 +3,30 @@ using UnityEngine;
 
 public class EnemyMoving : MonoBehaviour
 {
-    public float speed; // скорость пермещения персонажа 
-    public float turningSpeed; // скорость поворота персонажа 
-    public float targetDistance; // расстояние, на котором персонаж будет держатся от игрока
-    public float visionLength; // расстояние, на которое может смотреть
+    public float speed;
+    public float turningSpeed;
+    public float targetDistance; 
+    public float visionLength; 
     public GameObject target;
     const float eps = 0.1f;
 
     private Animator _animator;
 
 
-    void Moving(Transform character, Transform target) // перемещение персонажа
+    void Moving(Transform character, Transform target)
     {
-        // поворот в сторону цели
+        
         Vector3 direction = (target.position - character.position).normalized;
         Quaternion LookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Lerp(transform.rotation, LookRotation, Time.deltaTime * turningSpeed);
 
-        if (Vector3.Distance(target.position, character.position) <= visionLength && Vector3.Distance(target.position, character.position) > targetDistance) // Проверка расстояния до игрока 
+        if (Vector3.Distance(target.position, character.position) <= visionLength && Vector3.Distance(target.position, character.position) > targetDistance) // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ 
         {
             transform.position = Vector3.MoveTowards(character.position, target.position, Time.deltaTime * speed);
         }
         if (Vector3.Distance(target.position, character.position) < targetDistance - eps)
         {
-            // держится всегда перед "лицом" игрока, находясь на заданной дистанции
+            
             float angle = target.eulerAngles.y * (float) Math.PI/180;
             Vector3 backStep = new Vector3(target.position.x + targetDistance * (float)Math.Sin(angle), 0f, target.position.z + targetDistance * (float)Math.Cos(angle));
             transform.position = Vector3.MoveTowards(character.position, backStep, Time.deltaTime * speed);
@@ -44,7 +44,6 @@ public class EnemyMoving : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Vector3.Distance(gameObject.transform.position, target.transform.position) <= visionLength)
