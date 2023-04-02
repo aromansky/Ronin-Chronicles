d2c95596d79388bd060c3aa7ff86e26c8c496c0c
Koplik Dmitrii <koplikdima2004@gmail.com>
2023-04-02 22:04:00 +0300

    Update Menu

--- Ronin Chronicles/Assets/Scripts/PauseMenu.cs
+++ Ronin Chronicles/Assets/Scripts/PauseMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,7 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    public GameObject interfaceUI;
 
     void Update()
     {
@@ -25,14 +27,18 @@ public class PauseMenu : MonoBehaviour
     }
     public void Resume()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         pauseMenuUI.SetActive(false);
+        interfaceUI.SetActive(true);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
     public void Pause()
     {
+        Cursor.lockState = CursorLockMode.Confined;
         pauseMenuUI.SetActive(true);
+        interfaceUI.SetActive(false);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
@@ -42,5 +48,11 @@ public class PauseMenu : MonoBehaviour
         Debug.Log("Load");
         Time.timeScale = 1f;
         SceneManager.LoadScene("Menu");
+    }
+    
+    public void ExitGame()
+    {
+        Debug.Log("Игра закрылась");
+        Application.Quit();
     }
 }
